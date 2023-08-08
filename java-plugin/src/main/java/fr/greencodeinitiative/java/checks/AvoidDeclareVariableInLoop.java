74deabc47f1164a9dde212fb99c19b17161f5ad5
Thibault Azemar <thibault.azemar@thalesgroup.com>
2023-08-08 13:47:00 +0200

    Refactor rulekey

--- java-plugin/src/main/java/fr/greencodeinitiative/java/checks/AvoidDeclareVariableInLoop.java
+++ java-plugin/src/main/java/fr/greencodeinitiative/java/checks/AvoidDeclareVariableInLoop.java
@@ -12,15 +12,9 @@ import org.sonarsource.analyzer.commons.annotations.DeprecatedRuleKey;
 import java.util.Collections;
 import java.util.List;
 
-@Rule(
-        key = "ECVarInLoop",
-        name = "Performance",
-        description = "<p>Avoid declare variable in loop</p>",
-        priority = Priority.MINOR,
-        tags = {"bug"})
+@Rule(key = "ECVarInLoop")
 @DeprecatedRuleKey(repositoryKey = "greencodeinitiative-java", ruleKey = "ECVarInLoop")
 public class AvoidDeclareVariableInLoop extends IssuableSubscriptionVisitor {
-    public static final String RULE_KEY = "ECVarInLoop";
     public static final String MESSAGE_RULE = "Avoid declare variables in loops";
 
     @Override
@@ -32,10 +26,8 @@ public class AvoidDeclareVariableInLoop extends IssuableSubscriptionVisitor {
     public void visitNode(Tree tree){
         ForStatementTree forStatementTree = (ForStatementTree) tree;
         StatementTree body = forStatementTree.statement();
-
         if(body.is(Tree.Kind.BLOCK)){
             BlockTree blockTree = (BlockTree) body;
-
             blockTree.body().stream()
                     .filter(statement -> statement.is(Tree.Kind.VARIABLE))
                     .forEach(varDecl -> reportIssue(varDecl, MESSAGE_RULE));
